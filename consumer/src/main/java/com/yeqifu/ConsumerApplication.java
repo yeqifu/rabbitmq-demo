f7acf344bc9e31ed263a8ee4cf4730d4f63294cb
yeqifu <1784525940@qq.com>
2023-02-17 23:48:57 +0800

    rabbitmq简单队列模型

---
+++ consumer/src/main/java/com/yeqifu/ConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.yeqifu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: yeqifu
+ * @date: 2023/2/17 23:04
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
---
+++ publisher/src/main/java/com/yeqifu/PublisherApplication.java
@@ -0,0 +1,15 @@
+package com.yeqifu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: yeqifu
+ * @date: 2023/2/17 23:03
+ */
+@SpringBootApplication
+public class PublisherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PublisherApplication.class);
+    }
+}
