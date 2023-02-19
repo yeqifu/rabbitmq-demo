0f5bf914126b51b9af78296864f7e6b050f31f39
yeqifu <1784525940@qq.com>
2023-02-19 22:55:37 +0800

    完成修改序列化方式

--- consumer/src/main/java/com/yeqifu/ConsumerApplication.java
+++ consumer/src/main/java/com/yeqifu/ConsumerApplication.java
@@ -1,7 +1,10 @@
 package com.yeqifu;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author: yeqifu
@@ -11,5 +14,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
+    }
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
--- publisher/src/main/java/com/yeqifu/PublisherApplication.java
+++ publisher/src/main/java/com/yeqifu/PublisherApplication.java
@@ -1,7 +1,10 @@
 package com.yeqifu;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author: yeqifu
@@ -11,5 +14,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PublisherApplication {
     public static void main(String[] args) {
         SpringApplication.run(PublisherApplication.class);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
